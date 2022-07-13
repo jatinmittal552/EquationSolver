4d1712cc9363569e63f16f7a5532108e33350296
JatinMittal552 <jatinmittal552@gmail.com>
2022-07-14 01:11:21 +0530

    third

--- app/src/main/java/com/example/equationsolver/MainActivity2.java
+++ app/src/main/java/com/example/equationsolver/MainActivity2.java
@@ -9,19 +9,75 @@ import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    Button btn;
+    Button btna;
+    Button btnb;
+    Button btnc;
+    Button btnd;
+    Button btne;
+    Button btnf;
+    Button btng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        btn = findViewById(R.id.button);
+        btna = findViewById(R.id.btn1);
 
-        btn.setOnClickListener(new View.OnClickListener(){
+        btna.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent=new Intent(getBaseContext(),MainActivity3.class);
+                startActivity(intent);
+
+            }
+        });
+        btnb = findViewById(R.id.btn2);
+
+        btnb.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getBaseContext(),MainActivity4.class);
+                startActivity(intent);
+
+            }
+        });
+        btnc = findViewById(R.id.btn3);
+
+        btnc.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getBaseContext(),MainActivity5.class);
+                startActivity(intent);
+
+            }
+        });
+        btnd = findViewById(R.id.btn4);
+
+        btnd.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getBaseContext(),MainActivity6.class);
+                startActivity(intent);
+
+            }
+        });
+        btne = findViewById(R.id.btn5);
+
+        btne.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getBaseContext(),MainActivity7.class);
+                startActivity(intent);
+
+            }
+        });
+        btnf = findViewById(R.id.btn6);
+
+        btnf.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent=new Intent(getBaseContext(),MainActivity8.class);
                 startActivity(intent);
 
             }
